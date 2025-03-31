ab27cae8bda7486e20607dc739283f337233dc64
Bartlomiej Drapinski <g00428194@atu.ie>
2025-03-31 19:33:44 +0100

    Created ConstructorSuccess test

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PassengerTest {
     Passenger myPass;
 
@@ -7,6 +9,13 @@ public class PassengerTest {
     void setup()
     {
         myPass = new Passenger("Mr", "Bartek", "Drapinski");
+    }
+
+    void ConstructorSuccess()
+    {
+        assertEquals("Mr", myPass.getTitle());
+        assertEquals("Bartek", myPass.getFirstName());
+        assertEquals("Drapinski", myPass.getLastName());
     }
 
 }
