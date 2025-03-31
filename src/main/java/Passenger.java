7c5ea3987f4552f9b988bd8bcaf14e9fcfcf6237
Bartlomiej Drapinski <g00428194@atu.ie>
2025-03-31 19:46:39 +0100

    generated getters and setters for the 3 variables

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -10,4 +10,28 @@ public class Passenger {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
