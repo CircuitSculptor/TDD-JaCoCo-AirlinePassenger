6140c5e6985d99012642a6b760edb8a59246d9f4
Bartlomiej Drapinski <g00428194@atu.ie>
2025-03-31 19:51:32 +0100

    Created testTitle, testing for failure

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PassengerTest {
     Passenger myPass;
@@ -11,11 +13,19 @@ public class PassengerTest {
         myPass = new Passenger("Mr", "Bartek", "Drapinski");
     }
 
+    @Test
     void ConstructorSuccess()
     {
         assertEquals("Mr", myPass.getTitle());
         assertEquals("Bartek", myPass.getFirstName());
         assertEquals("Drapinski", myPass.getLastName());
+    }
+
+    @Test
+    void testTitle()
+    {
+        //Mr, Mrs or Ms
+        assertThrows(IllegalArgumentException.class, () -> {new Passenger("Dr", "Bartek", "Drapinski");});
     }
 
 }
