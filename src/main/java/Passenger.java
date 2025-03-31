5b559a89a172b00fe50d2fa44cdd77fbc551535e
Bartlomiej Drapinski <g00428194@atu.ie>
2025-03-31 19:37:01 +0100

    made passenger constructor

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,4 +1,9 @@
 public class Passenger {
 
+    private String title;
+    private String firstName;
+    private String lastName;
 
+    public Passenger(String title, String firstName, String lastName) {
+    }
 }
